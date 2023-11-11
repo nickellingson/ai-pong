579d32a40765ce638767e404b34b6d5b830bf322
Nicholas Ellingson <nickellingson8@gmail.com>
2023-11-11 15:30:51 -0600

    Add paddle movement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ pygame.init()
 # Set display
 # Variables
 WIDTH, HEIGHT = 700, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
 FPS = 60
@@ -19,6 +19,7 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 class Paddle:
 
     COLOR = WHITE
+    VELOCITY = 10
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -29,8 +30,14 @@ class Paddle:
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
+    def move(self, up=True):
+        if up:
+            self.y -= self.VELOCITY
+        else:
+            self.y += self.VELOCITY
 
-# Draw window function
+
+# Draw window function, takes in window and list of paddles, called in main function
 def draw(win, paddles):
     # Background
     win.fill(BLACK)
@@ -39,7 +46,25 @@ def draw(win, paddles):
     for paddle in paddles:
         paddle.draw(win)
 
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, WHITE, (WIDTH//2 - 5, i, 10, HEIGHT//20))
+
     pygame.display.update()
+
+
+# Takes in paddles and uses key strokes to move them, called in main function
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VELOCITY >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VELOCITY + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_i] and right_paddle.y - right_paddle.VELOCITY >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_k] and right_paddle.y + right_paddle.VELOCITY + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
 
 
 # Main function
@@ -56,7 +81,7 @@ def main():
         clock.tick(FPS)
 
         # Draw window
-        draw(WIN, [left_paddle, right_paddle])
+        draw(WINDOW, [left_paddle, right_paddle])
 
         for event in pygame.event.get():
 
@@ -64,9 +89,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
-    
+            
+            # Get keys pressed for paddle movement
+            keys = pygame.key.get_pressed()
+            handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
